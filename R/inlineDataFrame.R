3361cdae48b2cb1ff576887cce7aeb15ba30a67d
Romain François <romain@r-enthusiasts.com>
2014-12-30 14:07:47 +0100

    simplify inlineDataFrame

--- R/inlineDataFrame.R
+++ R/inlineDataFrame.R
@@ -1,15 +1,3 @@
-inlineDataFrame=function(str,header=TRUE,colClasses=NULL){
-  if(is.null(colClasses)){
-  df <- read.table(header=header,text=str)
-  }else{
-    df <- read.table(header=header,colClasses=colClasses,text=str)
-  }
-  
-#  if(header==T)
-#    df <- t(df)
-  
-#  if(header==F)
-# df <- c(tapply(df$V2, df$V1, as.vector))
-  
-  return(df)
+inlineDataFrame <- function(str,header=TRUE,colClasses=NA, ...){
+  read.table( text = str, header=header, colClasses=colClasses, ... )
 }
