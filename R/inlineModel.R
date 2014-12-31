68144025f8dffe27979f6111bd942e1e8f5fef3e
Romain François <romain@r-enthusiasts.com>
2014-12-31 11:40:22 +0100

    need to export inlineModel as it is used in examples

--- R/inlineModel.R
+++ R/inlineModel.R
@@ -1,3 +1,8 @@
+#' inline model
+#' 
+#' @param str model
+#' @param filename where to write the temporary model
+#' @export
 inlineModel <- function(str,filename="temp_model.txt"){
   write(str,filename)
   return(filename)
