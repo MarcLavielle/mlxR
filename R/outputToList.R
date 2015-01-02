49d3d98d0ea96fc0a6eef66d17b0da38ab1b12ad
MarcLavielle <Marc.Lavielle@gmail.com>
2015-01-02 10:17:56 +0100

    ensure retro-compatibility with mlxR2.0.0

--- R/outputToList.R
+++ R/outputToList.R
@@ -6,11 +6,9 @@ list <- structure(NA,class="result")
 "[<-.result" <- function(x,...,value) {
   args <- as.list(match.call())
   args <- args[-c(1:2,length(args))]
-  length(value) <- length(args)
   for(i in seq(along=args)) {
     a <- args[[i]]
-    if(!missing(a)) eval.parent(substitute(a <- v,list(a=a,v=value[[i]])))
+    eval.parent(substitute(a <- v,list(a=a,v=value)))
   }
   x
 }
-
