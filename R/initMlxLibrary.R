3861557f7c5c88f6061540d6003bf10c1c3bf62b
Romain François <romain@r-enthusiasts.com>
2015-01-07 11:05:10 +0100

    hiding the call to unlockBinding (R CMD check request)

--- R/initMlxLibrary.R
+++ R/initMlxLibrary.R
@@ -53,7 +53,8 @@ Otherwise, execute <Mlxlibrary PATH>/lib/mlxLibraryFirstLaunch.exe"
   #--- load Mlxlibrary
   mlxComputeRLibraryBuilder(mlxlibrary.path)
   
-  unlockBinding( "mlx_library_ready", NAMESPACE )
+  unlock <- unlockBinding
+  unlock( "mlx_library_ready", NAMESPACE )
   NAMESPACE[["mlx_library_ready"]] <- TRUE
   
 }
