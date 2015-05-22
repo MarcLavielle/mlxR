377c9f78b842fffc3457bc4f0b72cb18262a41b1
MarcLavielle <Marc.Lavielle@gmail.com>
2015-05-22 20:32:18 +0200

    seed created if not defined by the user

--- R/simulx.R
+++ R/simulx.R
@@ -81,9 +81,11 @@ simulx <- function(model=NULL,group=NULL,treatment=NULL,parameter=NULL,output=NU
   myOldENVPATH = Sys.getenv('PATH');
   initMlxLibrary()
   session=Sys.getenv("session.simulx")
-  Sys.setenv(LIXOFT_HOME=session)  
- 
- 
+  Sys.setenv(LIXOFT_HOME=session)
+  
+  if (is.null(settings$seed))
+    settings$seed <- round(as.numeric(Sys.time())*100)%%10000
+  
   #   Nmax <- 10
   test.group <- FALSE
   if ((!is.null(group))  & (is.null(settings$data.in)) & (is.null(settings$record.file))){
