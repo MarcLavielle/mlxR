0242fa916c43ea16decc166179ad37b6576452fc
MarcLavielle <Marc.Lavielle@gmail.com>
2015-10-19 19:09:56 +0200

    rename uniquemlx -> my.unique (conflict with kmplotmlx)

--- mlxR_demos/caseStudies/workflow/wftools.R
+++ mlxR_demos/caseStudies/workflow/wftools.R
@@ -3,8 +3,8 @@ addgroup <- function(r0, rv, str)
 {
   ic <- which(names(rv)==str)
   rs <- rv[order(rv[,3],rv[,1]),]
-  ur <- uniquemlx(rs[,3])
-  ui <- uniquemlx(rs[,1])
+  ur <- my.unique(rs[,3])
+  ui <- my.unique(rs[,1])
   ir <- data.frame(id=ui$value, group=as.factor(ur$sortIndex[ui$firstIndex]))
   ir <- ir[order(ir$id),]
   r1 <- merge(r0,ir,by="id")
@@ -12,7 +12,7 @@ addgroup <- function(r0, rv, str)
   return(r1)
 }
 
-uniquemlx <- function(x) 
+my.unique <- function(x) 
 { 
   d <- !duplicated(x) 
   u=list(value=x[d], firstIndex=which(d), sortIndex=match(x,x[d])) 
