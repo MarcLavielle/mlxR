116f4305cfaf5e044f1d69dedb9d0320c50e1fda
RaphaelKuate <you@example.com>
2015-12-24 08:54:37 +0100

    add object which close  mlxComputeR dll properly  after runs

---
+++ R/mlxComputeRCleaner.R
@@ -0,0 +1,17 @@
+mlxComputeR.Unload <- function (arg=NULL) {
+  dot_call <- .Call 
+  if(is.loaded('mlxComputeRUnload', PACKAGE='mlxComputeR'))
+  {
+    dot_call("mlxComputeRUnload", PACKAGE = "mlxComputeR") 
+  }
+}
+cleanerEnv<-environment()
+mlxComputeRcleanUp<- setRefClass("mlxComputeRcleanUp",
+                        methods = list(
+                          initialize = function(...) {                     
+                            reg.finalizer(cleanerEnv,mlxComputeR.Unload,onexit =TRUE)}
+                        ))
+
+.onLoad<-function(libname, pkgname){ 
+    mlxComputeRCleaner <-mlxComputeRcleanUp$new()
+}
