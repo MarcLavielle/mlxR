d0bff8859c721eb648c994005dea76673fde6225
RaphaelKuate <you@example.com>
2016-02-01 11:38:22 +0100

    set correct the name of error model in paramfile, it can change  in the V2 model  if user's parameter name is the same as error model name

--- R/monolix2simulx.R
+++ R/monolix2simulx.R
@@ -95,6 +95,10 @@ monolix2simulx <-function(project,parameter=NULL,group=NULL,open=FALSE,r.data=TR
       outfile = file.path(Rproject,paste0("/populationParameter.txt"))      
       cat(paste0("populationParameter<- read.vector('populationParameter.txt') \n"), file =projectExe, fill = FALSE, labels = NULL, append = TRUE) 
       write.table(populationParameter,file=outfile,col.names=FALSE,quote=FALSE)
+      ## edit populationParameter.txt, check and  set correct name of error moel,  it can change  in the V2 model
+      
+      paramfile<-outfile
+      setErrorModelName(paramfile,model)
       if (!is.null(param.list))
         param.list <- paste(param.list,"populationParameter",sep=",")  
       else
@@ -191,7 +195,7 @@ monolix2simulx <-function(project,parameter=NULL,group=NULL,open=FALSE,r.data=TR
     for( i in seq(1:length(regModelNamesTable))){
       if(!identical(regModelNamesTable[i],"")&&!length(grep("=",regModelNamesTable[i],fixed=TRUE,value=TRUE))
          &&!length(grep("regressor",regModelNamesTable[i],fixed=TRUE,value=TRUE))
-         ){
+      ){
         regModelNames<-c(regModelNames,regModelNamesTable[i])
         nbModelreg = nbModelreg +1
       }
@@ -201,10 +205,10 @@ monolix2simulx <-function(project,parameter=NULL,group=NULL,open=FALSE,r.data=TR
     for( i in seq(1:length(namesReg))){
       if(!identical(tolower(namesReg[i]),"id") &&
            !identical(tolower(namesReg[i]),"time")){
-             namesReg[i] <- regModelNames[iregModel]
-             
-             iregModel <-iregModel +1 
-             nbregOrig <- nbregOrig +1
+        namesReg[i] <- regModelNames[iregModel]
+        
+        iregModel <-iregModel +1 
+        nbregOrig <- nbregOrig +1
       }
     }
     if(nbregOrig +1 !=  iregModel)
@@ -255,4 +259,92 @@ monolix2simulx <-function(project,parameter=NULL,group=NULL,open=FALSE,r.data=TR
     setwd(mypath)
   }
   return(projectExe)
+}
+
+## set correct the name of error model in paramfile, it can change  in the V2 model
+## if user's parameter is the same as error model name
+
+
+setErrorModelName<- function(paramfile,model)
+{
+  modelread <- readLines(model)
+  errorline<-grep("errorModel",modelread)
+  errorused<-NULL
+  for(i in seq(1:length(errorline)))
+  {    
+    comment<-";"
+    line<-strsplit(modelread[errorline[i]],comment)[[1]][1]    
+    if(length(line))
+    {       
+      testerr<-strsplit(line,"errorModel")
+      if(length(testerr[[1]])==2)
+      {          
+        errorsub<-strsplit(testerr[[1]][2],'[/(/)]',perl=TRUE)
+        errorargs<-strsplit(errorsub[[1]][2],',')
+        for(ee in seq(1:length(errorargs[[1]])))
+        {
+          errorused<-c(errorused,trimws(errorargs[[1]][ee]))
+        }
+      }        
+      
+    }
+  }
+  ## todo replace names without _ in paramfile
+  
+  replaced=FALSE
+  endFlag<-"_"
+  if(length(errorused))
+  {
+    paramread <- readLines(paramfile)
+    for(i in seq(1:length(errorused)))
+    {
+      erri<-errorused[i]
+      erriChars <- strsplit(erri,"")[[1]]
+      lastChar <-  erriChars[length(erriChars)]
+      if(identical(lastChar,endFlag))
+      {
+        errifirstchars<-strsplit(erri,'(.$)',perl=TRUE)[[1]]
+        erriline<-paste0("^",erri,"$")
+        errparamf<-grep(erriline,paramread,perl=TRUE)
+        if(!length(errparamf))
+        {
+          for(i in seq(1:length(paramread)))
+          { 
+            paramname<- NULL
+            linesplitted<-strsplit(paramread[i],'\\s',perl=TRUE)
+            is<-1
+            if(length(linesplitted[[1]]))
+            {
+              for(j in seq(1:length(linesplitted[[1]])))
+              {
+                if(!identical(linesplitted[[1]][j],""))
+                {
+                  paramname<-linesplitted[[1]][j]
+                  is<-j
+                  break
+                }
+              }
+            }            
+            if(identical(paramname,errifirstchars))
+            {            
+              paramread[i]<-sub(linesplitted[[1]][is],erri,paramread[i]) 
+              replaced=TRUE 
+              break
+            }          
+          }
+        }
+      }
+    }
+  }
+  if(replaced==TRUE)
+  {
+    tmpFile<-paste0(paramfile,"_tmp")
+    file.rename(paramfile,tmpFile)
+    cat("",file =paramfile, fill = FALSE, labels = NULL, append = FALSE)    
+    for(i in seq(1:length(paramread)))
+    {
+      cat(paste0(paramread[i],"\n"),file =paramfile, fill = FALSE, labels = NULL, append = TRUE)   
+    }
+    unlink(tmpFile)
+  }
 }
