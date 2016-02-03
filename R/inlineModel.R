943d2395b81537c0fdfc4a2266b50b41789c6efa
RaphaelKuate <you@example.com>
2016-02-03 11:45:59 +0100

    add a unique id to  the temp_model file name, to avoid conflicts with diferrent instances in the same directory using  the same model file

--- R/inlineModel.R
+++ R/inlineModel.R
@@ -3,7 +3,12 @@
 #' @param str model
 #' @param filename where to write the temporary model
 #' @export
-inlineModel <- function(str,filename="temp_model.txt"){
+inlineModel <- function(str,filename=NULL){
+  if(is.null(filename))
+  {    
+    uuidd<-uuid()
+    filename <-paste0("temp_model-",uuidd,".txt")
+  }
   write(str,filename)
   return(filename)
 }
