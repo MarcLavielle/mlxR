d44fac5a66a8b3782f182a73816dd6865b104941
MarcLavielle <Marc.Lavielle@gmail.com>
2016-02-24 01:11:51 +0100

    summary for time to event data

--- R/statmlx.R
+++ R/statmlx.R
@@ -1,105 +1,108 @@
-#' @export
-statmlx <- function(r, stat, probs)
+# ' @export
+
+
+statmlx <- function(r, FUN="mean", probs=c(0.05, 0.5, 0.95), 
+                    type="continuous", surv.time=NULL)
 {
-  list.stat <- c("mean","sd","median","var") 
-  l.stat <- length(stat)
-  n.stat <- NULL
-  for (j in (1:l.stat)){
-    if (stat[j] %in% list.stat)
-      n.stat <- c(n.stat,stat[j])
-    else 
-      n.stat <- c(n.stat,paste0("p",probs*100))
-  }
+  if (any(!(FUN %in% c("mean","sd","median","var","quantile"))))
+    stop("\n\n possible values for 'FUN' are {'mean','sd','median','var','quantile'} ")
+  if (any(!(type %in% c('continuous','event'))))
+    stop("\n\n possible values for 'type' are {'continuous','event'} ")
+  # if (any(type=="survival") && is.null(time)) 
+  #   stop("\n\n a time value, or a vector of times, should be provided when 'survival' is defined as 'FUN'")
   
   r <- r[!is.na(names(r))]
-  rs <- list()  
-  for (j in (1:length(r))){
-    rj <- r[[j]]  
-    rj <- rj[,!colnames(rj) == "id"]
-    srj <- NULL
-    if (names(r)[j]=="parameter"){
-      if (is.null(rj$group)){
-        n.srj <- NULL
-        for (k in (1:l.stat)){
-          stat.k <- stat[k]       
-          if (stat.k %in% list.stat){
-            srj <- rbind(srj,sapply(rj,stat.k))
-            n.srj <- rbind(n.srj,paste0(names(rj),".",stat.k))
-          } else {
-            srj <- rbind(srj,sapply(rj,stat.k,probs=probs))
-            aa <- apply(expand.grid(names(rj),paste0("p",probs*100)), 1, paste, collapse=".")  
-            n.srj <- rbind(n.srj,t(matrix(aa,nrow=ncol(rj))))
-          }
-        }
-        srj <- as.vector(srj)
-        names(srj) <- n.srj
-        srj <- as.data.frame(t(srj))
-      } else {
-        ng <- length(unique(rj$group))
-        n.srj <- NULL
-        for (ig in (1:ng)) {
-          srjg <- NULL
-          rjg <- rj[rj$group==ig,-1]
-          for (k in (1:l.stat)){
-            stat.k <- stat[k]       
-            if (stat.k %in% list.stat){
-              srjg <- rbind(srjg,sapply(rjg,stat.k))
-              if (ig==1)
-                n.srj <- rbind(n.srj,paste0(names(rjg),".",stat.k))
-            } else {
-              srjg <- rbind(srjg,sapply(rjg,stat.k,probs=probs))
-              if (ig==1){
-                aa <- apply(expand.grid(names(rjg),paste0("p",probs*100)), 1, paste, collapse=".")  
-                n.srj <- rbind(n.srj,t(matrix(aa,nrow=ncol(rjg))))
-              }
-            }
-          }
-          srj <- rbind(srj, c(ig,as.vector(srjg)))
-        }
-        srj <- as.data.frame(srj)
-        names(srj) <- c("group",as.vector(n.srj))
+  
+  if (type=="event"){
+    r <- r[r$time>0,]
+    it <- which(names(r) == "time")
+    ie <- which(!(names(r) %in% c("pop","rep","id","time","group")))
+    ig <- r[-c(it,ie)]
+    uid <-  cumsum(!duplicated(ig)) 
+    y <- cbind(uid,r)
+    
+    surv.time <- c(100,200)
+    rrr <- list(uid)
+    ev.nb <- aggregate(r[ie], by=rrr, "sum")
+    names(ev.nb)[2] <- "nbEv"
+    rev <- cbind(unique(ig), ev.nb[2]) 
+    nev <- names(rev)
+    ev.min <- aggregate(r[it], by=rrr, "min")
+    if (!is.null(surv.time)){
+      for (k in (1:length(surv.time))){
+        survk <- (ev.min$time >= surv.time[k])*1
+        rev   <- cbind(rev, survk)
       }
-      
-    } else {
-      
-      ny0 <- names(r)[j]
-      iy <- which(names(rj) == ny0)
-      names(rj)[iy] <- "y"
-      for (k in (1:l.stat)){
-        stat.k <- stat[k]
-        if (is.null(rj$group)){
-          if (stat.k %in% list.stat)
-            srjk <- aggregate(y~time,data=rj,stat.k)
-          else 
-            srjk <- aggregate(y~time,data=rj,stat.k,probs=probs)
-          if (k==1)
-            srj <- as.data.frame(cbind(srjk[[1]],srjk[[2]]))
-          else
-            srj <- cbind(srj,srjk[[2]])
-        } else {
-          if (stat.k %in% list.stat)
-            srjk <- aggregate(y~group+time,data=rj,stat.k)
-          else 
-            srjk <- aggregate(y~group+time,data=rj,stat.k,probs=probs)      
-          if (k==1)
-            srj <- as.data.frame(cbind(srjk[[1]],srjk[[2]],srjk[[3]]))
-          else
-            srj <- cbind(srj,srjk[[3]])
-        }
-      }
-      
-      srj <- as.data.frame(srj)
-      if (is.null(rj$group))
-        names(srj) <- c("time",paste0(ny0,".",n.stat))
-      else {
-        names(srj) <- c("group","time",paste0(ny0,".",n.stat))
-        srj <- srj[order(srj$group),]
-      }
+      nev <- c(nev,paste0("S",surv.time))
     }
-    rownames(srj) <- NULL
-    rs[[j]] <- srj
+    names(rev) <- nev
+    # ev.t1 <- ev.min
+    # ev.t1[ev.nb==0] <- NA
+    
+    r <- rev
   }
-  return(rs)
+  
+  list.FUN <- c("mean","sd","median","var") 
+  l.FUN <- length(FUN)
+  n.FUN <- NULL
+  for (j in (1:l.FUN)){
+    if (FUN[j] %in% list.FUN)
+      n.FUN <- c(n.FUN,FUN[j])
+    else 
+      n.FUN <- c(n.FUN,paste0("p",probs*100))
+  }
+  
+  rrr <- list()
+  nr <- NULL
+  if (!is.null(r$pop)){
+    rrr[[length(rrr)+1]] <- r$pop 
+    nr <- c(nr, "pop")
+  }
+  if (!is.null(r$rep)){
+    rrr[[length(rrr)+1]] <- r$rep 
+    nr <- c(nr, "rep")
+  }
+  if (!is.null(r$time)){
+    rrr[[length(rrr)+1]] <- r$time 
+    nr <- c(nr, "time")
+  }
+  if (!is.null(r$group)){
+    rrr[[length(rrr)+1]] <- r$group 
+    nr <- c(nr, "group")
+  }  
+  r$pop <- r$rep <- r$group <- r$time <- r$id <- NULL
+  
+  sr <- NULL
+  for (j in (1:l.FUN)){
+    statj <- FUN[j]
+    if (statj %in% list.FUN) {
+      if (length(rrr)>0)
+        X <- aggregate(r, by=rrr, statj)
+      else
+        X <- as.data.frame(t(sapply(r,statj)))
+      n.srj <- paste0(names(r),".",statj)
+    } else {
+      if (length(rrr)>0){
+        aX <- aggregate(r, by=rrr, statj, probs=pr)
+        nX <- length(aX)
+        X <- NULL
+        for (k in (1:nX))
+          X <- cbind(X, aX[[k]])
+        X <- as.data.frame(X)
+      } else {
+        aX <- sapply(r,statj, probs=pr)
+        X <- as.data.frame(t(as.vector(aX)))
+      }
+      aa <- apply(expand.grid(names(r),paste0("p",probs*100)), 1, paste, collapse=".")  
+      n.srj <- t(matrix(aa,nrow=ncol(r)))
+    }
+    names(X) <- c(nr,n.srj)
+    if (j==1)
+      sr <- X
+    else 
+      sr <- merge(sr, X)
+  }
+  return(sr)
 }
 
 
