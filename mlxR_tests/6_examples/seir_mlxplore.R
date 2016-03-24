aa1d708239bc5faa31eaacb2d7cbbfb3e3b98595
MarcLavielle <Marc.Lavielle@gmail.com>
2016-03-24 23:53:11 +0100

    remove mlxR_tests

--- mlxR_tests/6_examples/seir_mlxplore.R
+++
@@ -1,9 +0,0 @@
-setwd(dirname(parent.frame(2)$ofile))
-
-
-p <- list(name=c('A','lambda','gamma','epsilon','d','omega','tau'), 
-          value=c(0.5, 0.4, 0.02, 0.1, 0.01, 0.3, 60))
-
-out <- list(name=c('S', 'E', 'I', 'R'), time=-20:350)
-
-mlxplore(model='seir_model.txt', parameter=p, output=out)
