42aa25a1e98d3f858a1228a454f943ccc15c9656
MarcLavielle <Marc.Lavielle@inria.fr>
2017-11-09 11:55:38 +0100

    add comment add filename = "random"

--- R/inlineModel.R
+++ R/inlineModel.R
@@ -1,15 +1,45 @@
 #' inline model
 #' 
+#' Define a model "inline"
+#' 
+#' A temporary model file \code{filename} is created.  Default name is "temp_model.txt". 
+#' \code{filename="random"} generates a random name.
+#'  
 #' @param str model
-#' @param filename where to write the temporary model
+#' @param filename name of the temporary model file 
+#' 
+#' @return A Shiny app with files ui.R, server.R and model.txt
+#' @examples
+#' \dontrun{
+#' myModel1 <- inlineModel("
+#' [LONGITUDINAL]
+#' EQUATION:
+#' f = 10*exp(-0.2*t)
+#' ")
+#' 
+#' print(myModel1)
+#' 
+#' r <- simulx(model=myModel1, output=list(name="f", time=0:100))
+#' 
+#' myModel2 <- inlineModel("
+#' [LONGITUDINAL]
+#' EQUATION:
+#' f = 10*exp(-0.2*t)
+#' ", filename="random")
+#' 
+#' print(myModel2)
+#' }
 #' @export
-inlineModel <- function(str,filename=NULL){
-  if(is.null(filename)) {
-    uuidd<-uuid()
-    filename <-paste0("tempModel_",uuidd,".txt")
-    return(list(filename=filename, str=str))
+inlineModel <- function(str, filename=NULL) {
+  if (is.null(filename)) {
+    r <- list(filename="tempModel.txt", str=str)
+  } else if (identical(filename, "random")) {
+    uuiidd <- uuid()
+    filename <-paste0("tempModel_",uuiidd,".txt")
+    r <- list(filename=filename, str=str)
   } else {
     write(str, filename)
-    return(filename)
+    r <- filename
   }
+  return(r)
 }
