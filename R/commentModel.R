cfb78274a074069dd093fde9de5ac30011aa70c6
Marc Lavielle <Marc.Lavielle@inria.fr>
2020-08-20 16:18:52 +0200

    bug fix 4.1.5

--- R/commentModel.R
+++ R/commentModel.R
@@ -2,6 +2,7 @@ commentModel  <-  function(model, parameters, test.project) {
   
   sections <- c("COVARIATE", "INDIVIDUAL","POPULATION")
   terms = splitModel( model,sections) 
+  terms <- arrangeBlocs(terms)
   nmod <- NULL
   lines <- NULL
   i1 <- NULL
@@ -42,4 +43,34 @@ commentModel  <-  function(model, parameters, test.project) {
   
   return(list(model=model, test.project=test.project))
 }
+
+arrangeBlocs <- function(terms) {
+  for (k in 1:length(terms)) {
+    if (!is.null(terms[[k]]$lines)) {
+      stk <- splitSection(terms[[k]]) 
+      if (length(stk$blocks)>1) {
+        lk <- c(paste0("[",stk$name,"]"), stk$input)
+        j.eq <- grep("EQUATION:", stk$blocks)
+        if (length(j.eq)>0) {
+          lk <- c(lk, "EQUATION:")
+        for (i in (1:length(j.eq)))
+          lk <- c(lk, stk$lines[[j.eq[i]]])
+        }
+        j.def <- grep("DEFINITION:", stk$blocks)
+        if (length(j.def)>0)  {
+          lk <- c(lk, "DEFINITION:")
+          for (i in (1:length(j.def)))
+            lk <- c(lk, stk$lines[[j.def[i]]])
+        }
+        terms[[k]]$lines <- lk
+      }
+    }
+  }
+  return(terms)
+}
+
+
+
+
+
 
